0a5bb83c5b9aebada9989f0343255e189cfcf0e4
Anthraxmonki <jkrizenesky@yahoo.com>
2013-03-13 03:34:59 -0500

    Movement Works
    The Shuttle can move UpLEft, UpRight, DownRight, DownLEft, Up, Right,
    Down, and Left.

--- PlayerShip.cs
+++ PlayerShip.cs
@@ -28,16 +28,17 @@ namespace StarshipX
         const float fSpeed2 = 300f;
         const float fSpeed3 = 450f;
         const float fSpeed4 = 600f;
+        const float fSpeed5 = 750f;
 
-        const float fBaseSpeed = 140f;
+        const float fSpeedBase = 140f;
         float fSpeedModifier;
 
-        DirectonState currentDirection = DirectonState.NoDirection;
-        enum DirectonState
+        public DirectionState currentDirectionState = DirectionState.NoDirection;
+        public enum DirectionState
         {
-            UpLeft, Up, Upright,
+            UpLeft, Up, UpRight,
             Right,
-            DownRight,Down,DownLeft,
+            DownRight, Down, DownLeft,
             Left,
             NoDirection
         
@@ -50,7 +51,7 @@ namespace StarshipX
         {
 
             base.LoadContent(thecontentManager, sfileName, fgetsetScale);
-            fSpeedModifier = fSpeed4;
+            fSpeedModifier = fSpeed5;
 
         }
 
@@ -71,6 +72,10 @@ namespace StarshipX
 
         public void Update(GameTime thegameTime, KeyboardState kscurrentState, GamePadState gpcurrentState)
         {
+
+            SetDirectionState(kscurrentState, gpcurrentState);
+            MoveDirection(thegameTime);
+
 
 
             //GetDirectionInput(thegameTime, kscurrentState, gpcurrentState);
@@ -93,45 +98,156 @@ namespace StarshipX
 
 
 
+        //Multi-directionals MUST be updated first
+        public void SetDirectionState(KeyboardState kscurrentState, GamePadState gpcurrentState)
+        {
+            //
+            //UPLEFT UPLEFT UPLEFT UPLEFT
+            if (kscurrentState.IsKeyDown(Keys.Up) 
+                && kscurrentState.IsKeyDown(Keys.Left))
+            {
+                currentDirectionState = DirectionState.UpLeft;       
+            }
+            //
+            ////UPRIGHT UPRIGHT UPRIGHT UPRIGHT
+            else if (kscurrentState.IsKeyDown(Keys.Up)
+                && kscurrentState.IsKeyDown(Keys.Right))
+            {
+                currentDirectionState = DirectionState.UpRight;
+            }
+
+            //DOWNRIGHT DOWNRIGHT DOWNRIGHT DOWNRIGHT
+            else if (kscurrentState.IsKeyDown(Keys.Right)
+                && kscurrentState.IsKeyDown(Keys.Down))
+            {
+                currentDirectionState = DirectionState.DownRight;
+            }
+            
+            //DOWNLEFT DOWNLEFT DOWNLEFT DOWNLEFT
+            else if (kscurrentState.IsKeyDown(Keys.Left)
+                && kscurrentState.IsKeyDown(Keys.Down))
+            {
+                currentDirectionState = DirectionState.DownLeft;
+            }
+
+
+            //
+            //UP UP UP UP UP
+            else if (kscurrentState.IsKeyDown(Keys.Up))
+            {
+                currentDirectionState = DirectionState.Up;
+            }
+            //RIGHT RIGHT RIGHT RIGHT
+            else if (kscurrentState.IsKeyDown(Keys.Right))
+            {
+                currentDirectionState = DirectionState.Right;
+            }
+            //DOWN DOWN DOWN DOWN
+            else if (kscurrentState.IsKeyDown(Keys.Down))
+            {
+                currentDirectionState = DirectionState.Down;
+            }
+            //LEFT LEFT LEFT LEFT
+            else if (kscurrentState.IsKeyDown(Keys.Left))
+            {
+                currentDirectionState = DirectionState.Left;
+            }
+
+        }
 
 
 
-        //public void GetDirectionInput(GameTime thegameTime, KeyboardState kscurrentState, GamePadState gpcurrentState)
-        //{
-        //    if (kscurrentState.IsKeyDown(Keys.Left)
-        //        || gpcurrentState.DPad.Left == ButtonState.Pressed)
-        //    {
-        //        v2ShipPosition += new Vector2((-1 * (fBaseSpeed + fSpeedModifier))  * (float)(thegameTime.ElapsedGameTime.TotalSeconds), 0);
-        //    }
-        //    else if (kscurrentState.IsKeyDown(Keys.Right)
-        //        || gpcurrentState.DPad.Right == ButtonState.Pressed)
-        //    {
-        //        v2ShipPosition += new Vector2( (fBaseSpeed + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds), 0);
-        //    }
-
-        //    else if (kscurrentState.IsKeyDown(Keys.Up)
-        //        || gpcurrentState.DPad.Up == ButtonState.Pressed)
-        //    {
-        //        v2ShipPosition += new Vector2(0,    (-1 * (fBaseSpeed + fSpeedModifier)) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
-        //    }
-
-        //    else if (kscurrentState.IsKeyDown(Keys.Down)
-        //        || gpcurrentState.DPad.Down == ButtonState.Pressed)
-        //    {
-        //        v2ShipPosition += new Vector2(0, (fBaseSpeed + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
-        //    }
-
-
-
-
-        //}
 
 
 
 
 
+        public void MoveDirection(GameTime thegameTime)
+        {
+            //UPLEFT UPLEFT UPLEFT
+            if (currentDirectionState == DirectionState.UpLeft)
+            {
+                v2ShipPosition += new Vector2(
+                    -1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds),
+                    -1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
+
+                currentDirectionState = DirectionState.NoDirection;
+            }
+
+            //UPRIGHT UPRIGHT UPRIGHT UPRIGHT
+            else if (currentDirectionState == DirectionState.UpRight)
+            {
+                v2ShipPosition += new Vector2( 
+                    (fSpeedBase + fSpeedModifier)      * (float)(thegameTime.ElapsedGameTime.TotalSeconds), 
+                    -1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
+
+                currentDirectionState = DirectionState.NoDirection;
+            }
+
+            //DOWNRIGHT DOWNRIGHT DOWNRIGHT DOWNRIGHT
+            else if (currentDirectionState == DirectionState.DownRight)
+            {
+                v2ShipPosition += new Vector2(
+                    (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds),
+                    (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds)
+                    );
+                currentDirectionState = DirectionState.NoDirection;
+            }
+
+            //DOWNLOEFT DOWNLEFT DOWNLEFT DOWNLEFT
+            else if (currentDirectionState == DirectionState.DownLeft)
+            {
+                v2ShipPosition += new Vector2(
+                    -1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds),
+                    (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds)
+                    );
+                currentDirectionState = DirectionState.NoDirection;
+            }
 
 
+
+
+            //UP UP UP UP
+            else if (currentDirectionState == DirectionState.Up)
+            {
+                v2ShipPosition += new Vector2(
+                    0,
+                    -1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
+
+                currentDirectionState = DirectionState.NoDirection;
+            }
+            //RIGHT RIGHT RIGHT RIGHT
+            else if (currentDirectionState == DirectionState.Right)
+            {
+                v2ShipPosition += new Vector2((fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds), 0);
+                currentDirectionState = DirectionState.NoDirection;
+            
+            }
+            //DOWN DOWN DOWN DOWN
+            else if (currentDirectionState == DirectionState.Down)
+            {
+                v2ShipPosition += new Vector2(0, (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds));
+                currentDirectionState = DirectionState.NoDirection;
+            }
+            //LEFT LEFT LEFT LEFT
+            else if (currentDirectionState == DirectionState.Left)
+            {
+                v2ShipPosition += new Vector2(-1 * (fSpeedBase + fSpeedModifier) * (float)(thegameTime.ElapsedGameTime.TotalSeconds), 0);
+                currentDirectionState = DirectionState.NoDirection;
+            }
+
+
+
+
+        }
+
+
+    
+
+
+
+
+  
 
 
         public override void Draw(SpriteBatch thespriteBatch)
